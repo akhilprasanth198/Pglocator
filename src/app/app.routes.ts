35283a63574c1294387a014af2607417ad430206
Akhil <akhilprasanth007@gmail.com>
2024-09-26 14:43:33 +0530

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { PgSearchComponent } from './pg-search/pg-search.component';
+export const routes: Routes = [
+    {path:'',redirectTo: 'home', pathMatch: 'full'},
+    {path:'navbar',component:NavbarComponent},
+    {path:'login',component:LoginComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
+    
+];
