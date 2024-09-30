4a9d9ac0611bfd7b86123d0d3783ea2d4ba85ec8
haseebhpk <haseebhpk303@gmail.com>
2024-09-30 12:34:58 +0530

    owner action html

---
+++ src/app/admin/owner-action/owner-action.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-action',
+  standalone: true,
+  imports: [],
+  templateUrl: './owner-action.component.html',
+  styleUrl: './owner-action.component.css'
+})
+export class OwnerActionComponent {
+
+}
---
+++ src/app/admin/view-owner/view-owner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-owner',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-owner.component.html',
+  styleUrl: './view-owner.component.css'
+})
+export class ViewOwnerComponent {
+
+}
