13f7eeead149212c99368fdd5b77640ba8da49b7
haseebhpk <haseebhpk303@gmail.com>
2024-09-30 13:20:43 +0530

    view owner html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { OwnerActionComponent } from './admin/owner-action/owner-action.component';
 import { PgSearchComponent } from './pg-search/pg-search.component';
+import { ViewOwnerComponent } from './admin/view-owner/view-owner.component';
 export const routes: Routes = [
     {path:'',redirectTo: 'home', pathMatch: 'full'},
     {path:'home',component:HomepageComponent},
@@ -14,5 +15,6 @@ export const routes: Routes = [
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
     {path:'owner-action',component:OwnerActionComponent},
+    {path:'view-owner',component:ViewOwnerComponent},
     
 ];
