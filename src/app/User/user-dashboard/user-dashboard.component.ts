e8586f2fe7aa45d55e00bc23f2783875bb5f572c
Akhil <akhilprasanth007@gmail.com>
2024-10-03 13:56:09 +0530

    admin navbar change style

--- src/app/User/user-dashboard/user-dashboard.component.ts
+++ src/app/User/user-dashboard/user-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserNavbarComponent } from "../user-navbar/user-navbar.component";
+import { AdminSidebarComponent } from "../../admin/admin-sidebar/admin-sidebar.component";
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [],
+  imports: [UserNavbarComponent, AdminSidebarComponent],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
--- src/app/admin/admin-topbar/admin-topbar.component.ts
+++ src/app/admin/admin-topbar/admin-topbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-topbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './admin-topbar.component.html',
   styleUrl: './admin-topbar.component.css'
 })
 export class AdminTopbarComponent {
-
+onLogout(){
+  
+}
 }
