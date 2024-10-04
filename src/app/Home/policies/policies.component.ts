2971934bb411cd46707a5f34998d4f8dde308765
Akhil <akhilprasanth007@gmail.com>
2024-10-04 09:53:51 +0530

    privacy policy

--- src/app/Home/policies/policies.component.ts
+++ src/app/Home/policies/policies.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PolicyComponent } from "./policy/policy.component";
+import { PrivacyComponent } from './privacy/privacy.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-policies',
   standalone: true,
-  imports: [],
+  imports: [PolicyComponent,PrivacyComponent,RouterOutlet,RouterLink],
   templateUrl: './policies.component.html',
   styleUrl: './policies.component.css'
 })
