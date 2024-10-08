f33ba101904fca5787dd9159fda7fc123dbcb2fe
ASWINRATHEESH04 <4swin2404@gmail.com>
2024-10-08 13:22:08 +0530

    about and contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Home/navbar/navbar.component';
 import { Router } from '@angular/router';
+import { ContactComponent } from "./Home/contact/contact.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgIf,NavbarComponent,CommonModule],
+  imports: [RouterOutlet, NgIf, NavbarComponent, CommonModule, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
